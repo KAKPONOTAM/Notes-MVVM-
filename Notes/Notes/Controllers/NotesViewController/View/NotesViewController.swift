a7dbfda64f23ec651b26c7955f1474a4ef906de7
KAKPONOTAM <sabitovdanill@icloud.com>
2022-12-28 22:53:51 +0700

    configure
    configure pods

--- Notes/Notes/Controllers/NotesViewController/View/NotesViewController.swift
+++ Notes/Notes/Controllers/NotesViewController/View/NotesViewController.swift
@@ -2,9 +2,24 @@ import UIKit
 
 final class NotesViewController: UIViewController {
     internal var viewModel: NotesViewModel?
+    private lazy var notesTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.dataSource = self
+        tableView.delegate = self
+        return tableView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 }
 
+extension NotesViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return .zero
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
